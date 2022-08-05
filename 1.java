988b81b32652615fec512105718997114e10c0fe
CapstoneGL11 <110578182+CapstoneGL11@users.noreply.github.com>
2022-08-05 08:13:34 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -2,6 +2,5 @@ class sample{
 	public static void main(Strin args[]){
 		System.out.println("Hello Capstone!!");
 		System.out.println("Hai!");
->>>>>>> 657250b6675f75fce8b04e10e5ef75b23c5439fc
 	}
 }
